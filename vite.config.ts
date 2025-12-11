c9c9801048648ad7a9a2a44d2dfc632d66982188
delnic85 <delnic85@gmail.com>
2025-12-11 18:08:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: "/ECO-Tour/", // <--- AGGIUNGI QUESTA RIGA (il nome esatto del tuo repo tra slash)
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
